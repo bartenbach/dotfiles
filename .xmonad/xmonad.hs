630c6a6f2a82ce52bec5c2033245e4fa65cc5a2a
Blake Bartenbach <SeeD419@gmail.com>
2012-12-14 12:47:26 -0600

    Adding a few extra dotfiles

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,15 +1,103 @@
-import XMonad
-
+{-# xmonad.hs - Haskell configuration file for XMonad #-}
+import XMonad hiding ((|||))
+import System.Exit (exitWith, ExitCode(ExitSuccess))
 import XMonad.Layout.Tabbed
+import XMonad.Layout.LayoutCombinators ((|||), JumpToLayout(..))
 import XMonad.Layout.Grid
---import XMonad.Layout.Mirror
-import XMonad.Layout.NoBorders
 import XMonad.Util.Themes
+import XMonad.Util.EZConfig (additionalKeys)
+import qualified XMonad.StackSet as W
 
--- Themes: defaultTheme,smallClean,robertTheme,deiflTheme,oxymor00nTheme
--- Theme options - noBorders
-customlayoutHook = Full ||| tabbed shrinkText (theme deiflTheme) ||| Grid ||| Tall 1 0.03 0.68
+myMod = mod1Mask
+myShiftMod = myMod .|. shiftMask
+killXMask = myMod .|. controlMask
 
-main = xmonad $ defaultConfig
-    { terminal = "urxvtc",
-      layoutHook = customlayoutHook }
+myWorkspaces = ["1","2","3","4","5","6","7","8","9"]
+
+myTheme = robertTheme
+
+myBorderWidth        = 2
+myNormalBorderColor  = "#000000"
+myFocusedBorderColor = "#ff0000"
+
+myFocusFollowsMouse :: Bool
+myFocusFollowsMouse = False
+
+
+data Applications = MyTerminal
+                  | Screenshot
+                  | ScreenshotArea
+                  | MyMenu
+
+instance Show Applications where
+  show MyTerminal     = "urxvtc"
+  show Screenshot     = "scrot -q100 '%m-%d-%Y.png' -e 'mv $f ~/img/scrots'"
+  show ScreenshotArea = "scrot -q100 -s '%m-%d-%Y.png' -e 'mv $f ~/img/scrots'"
+  show MyMenu         = "dmenu_run"
+
+
+launch = spawn . show
+
+myKeys =
+    [ ((myMod, xK_Return),          launch MyTerminal)
+    , ((myMod, xK_p),               launch MyMenu)
+    , ((myMod, xK_c),               kill)
+    , ((myMod, xK_space),           sendMessage NextLayout)
+    , ((myMod, xK_Up),              windows W.focusUp)
+    , ((myMod, xK_Down),            windows W.focusDown)
+    , ((myMod, xK_Left),            windows W.focusUp)
+    , ((myMod, xK_Right),           windows W.focusDown)
+    , ((myMod, xK_j),               windows W.focusUp)
+    , ((myMod, xK_semicolon),       windows W.focusDown)
+    , ((myMod, xK_k),               windows W.focusDown)
+    , ((myMod, xK_dollar),          windows W.swapMaster)
+    , ((myMod, xK_h),               sendMessage Shrink)
+    , ((myMod, xK_l),               sendMessage Expand)
+    , ((myMod, xK_t),               withFocused $ windows . W.sink)
+    , ((myMod, xK_comma),           sendMessage (IncMasterN 1)) 
+    , ((myMod, xK_ampersand),       sendMessage (IncMasterN (-1)))
+    , ((myMod, xK_q),               spawn "xmonad --recompile; xmonad --restart")
+    , ((myShiftMod, xK_j),          windows W.swapDown)
+    , ((myShiftMod, xK_semicolon),  windows W.swapUp) 
+    , ((myShiftMod, xK_Print),      launch ScreenshotArea)
+    , ((myMod, xK_Print),           launch Screenshot)
+    , ((killXMask, xK_BackSpace),   io (exitWith ExitSuccess)) 
+    ]
+    ++
+
+    [((m .|. myMod, k), windows $ f i)
+        | (i, k) <- zip (myWorkspaces) [xK_1 .. xK_9]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+    ++
+
+    [((m .|. myMod, key), screenWorkspace sc >>= flip whenJust (windows . f))
+        | (key, sc) <- zip [xK_e, xK_w] [0..]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+
+myLayout = tiled ||| tabbed shrinkText (theme myTheme) ||| Grid ||| Full 
+  where
+     tiled   = Tall nmaster delta ratio
+     nmaster = 1
+     ratio   = 1/2
+     delta   = 3/100
+
+myManageHook = composeAll
+    [ className =? "MPlayer"        --> doFloat
+    , className =? "Gimp"           --> doFloat
+    , resource  =? "desktop_window" --> doIgnore
+    , resource  =? "kdesktop"       --> doIgnore 
+    ]
+
+main = xmonad defaults
+
+defaults = defaultConfig {
+        modMask = myMod,
+        terminal = show MyTerminal,
+        focusFollowsMouse = myFocusFollowsMouse,
+        borderWidth = myBorderWidth,
+        workspaces = myWorkspaces,
+        normalBorderColor = myNormalBorderColor,
+        focusedBorderColor = myFocusedBorderColor,
+        layoutHook = myLayout,
+        manageHook = myManageHook
+    } `additionalKeys` myKeys
